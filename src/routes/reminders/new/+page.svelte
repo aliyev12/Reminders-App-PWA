1281321707282f78264dd3817e57b18f338af6de
Abdul Aliyev <aaa7c4@gmail.com>
2025-12-03 22:00:13 -0500

    Implement browser native date picker

--- src/routes/reminders/new/+page.svelte
+++ src/routes/reminders/new/+page.svelte
@@ -2,7 +2,6 @@
 	import type { PageData } from './$types';
 	import { superForm } from 'sveltekit-superforms/client';
 	import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
-	import DatePicker from '$lib/components/reminders/DatePicker.svelte';
 
 	export let data: PageData;
 
@@ -17,7 +16,18 @@
 		taintedMessage: true,
 		dataType: 'json'
 	});
-	console.log('message  ', $message);
+
+	let localDateString: string | undefined = $formData.date?.substring(0, 16) ?? undefined;
+
+	$: {
+		if (localDateString) {
+			const date = new Date(localDateString);
+			$formData.date = date.toISOString();
+		} else {
+			$formData.date = '';
+		}
+	}
+
 	$: isSubmitting = $submitting;
 </script>
 
@@ -78,7 +88,52 @@
 			</div>
 		</div>
 
-		<DatePicker />
+		<div>
+			<label for="date" class="mb-2 block text-sm font-medium text-gray-700 dark:text-neutral-300">
+				Reminder Date and Time
+			</label>
+			<div class="relative">
+				<input
+					type="datetime-local"
+					id="date"
+					name="date"
+					class="block w-full rounded-lg border px-4 py-2.5 focus:border-blue-500 focus:ring-blue-500
+                                sm:py-3 sm:text-sm
+                                {$errors.date
+						? 'border-red-500 focus:border-red-500 focus:ring-red-500'
+						: 'border-gray-300 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-400'}"
+					bind:value={localDateString}
+					aria-invalid={$errors.date ? 'true' : undefined}
+					aria-describedby="date-error"
+					{...$constraints.date}
+				/>
+				{#if $errors.date}
+					<div class="pointer-events-none absolute inset-y-0 end-0 flex items-center pe-3">
+						<svg
+							class="size-4 shrink-0 text-red-500"
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<circle cx="12" cy="12" r="10"></circle>
+							<line x1="12" x2="12" y1="8" y2="12"></line>
+							<line x1="12" x2="12.01" y1="16" y2="16"></line>
+						</svg>
+					</div>
+				{/if}
+			</div>
+			{#if $errors.date}
+				<p class="mt-2 text-sm text-red-600" id="date-error">
+					{$errors.date}
+				</p>
+			{/if}
+		</div>
 
 		<button class="btn-primary" type="submit" disabled={isSubmitting}>
 			{#if isSubmitting}
